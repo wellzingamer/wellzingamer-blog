2452ce82f5c9a3031a0cd94b7272ddc1d28cea6d
wellzingamer <particawelson@gmail.com>
2025-04-07 22:59:18 -0300

    add

---
+++ scrypt.js
@@ -0,0 +1,5 @@
+// Exemplo simples de interatividade
+document.addEventListener('DOMContentLoaded', function() {
+    // Aqui você pode adicionar funcionalidades com JavaScript, como animações, formulários dinâmicos, etc.
+    console.log("Bem-vindo ao Wellzingamer Blog!");
+});
